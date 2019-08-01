2650ef89e34e470b73c05819ce988bd6470efa1d
danielhelfand <helfand.4@gmail.com>
2019-08-01 11:39:22 -0400

    change resource budget to custom, add resource requests to s2i task

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-openshift-pipelines-with-tekton
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-openshift-pipelines-with-tekton:master
-RESOURCE_BUDGET=medium
+RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
