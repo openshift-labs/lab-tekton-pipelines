13ca69c02d53b16f840277984d47ce62eea8d6fe
danielhelfand <helfand.4@gmail.com>
2019-08-01 09:52:43 -0400

    adding settings and limit range settings

---
+++ .workshop/settings.sh
@@ -0,0 +1,5 @@
+WORKSHOP_NAME=lab-openshift-pipelines-with-tekton
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-openshift-pipelines-with-tekton:master
+RESOURCE_BUDGET=medium
+MAX_SESSION_AGE=3600
+IDLE_TIMEOUT=300
