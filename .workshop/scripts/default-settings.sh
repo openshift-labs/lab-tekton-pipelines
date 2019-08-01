2e38dceecaacb44dc1d981a6a00573a261dffe6c
danielhelfand <helfand.4@gmail.com>
2019-08-01 12:19:33 -0400

    update console image to 4.2

--- .workshop/scripts/default-settings.sh
+++ .workshop/scripts/default-settings.sh
@@ -1,7 +1,7 @@
 TERMINAL_IMAGE=quay.io/openshiftlabs/workshop-terminal:2.12.2
 DASHBOARD_IMAGE=quay.io/openshiftlabs/workshop-dashboard:3.7.1
 SPAWNER_IMAGE=quay.io/openshiftlabs/workshop-spawner:4.4.0
-CONSOLE_IMAGE=quay.io/openshift/origin-console:4.1
+CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 
 SPAWNER_REPO=openshift-labs/workshop-spawner
 SPAWNER_VERSION=4.4.0
