45b331b4f94b48d04602d691b7d33ff87250f0c7
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-08-03 20:59:01 +1000

    Want to use 4.2 web console for developer view.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,6 @@
 WORKSHOP_NAME=lab-tekton-pipelines
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-openshift-pipelines-with-tekton:master
+CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
