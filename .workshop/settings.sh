e18bb0364fb3e6819d902ccc00a5bfdf259c1f63
Jorge Morales <jorgemoralespou@gmail.com>
2019-08-26 20:08:46 +0200

    Adding scripts to install operator programatically

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-tekton-pipelines
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:1.0
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:latest
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=7200
