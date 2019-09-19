829c163dc28962155fc8f37bdec43b9cd2f0eb12
danielhelfand <helfand.4@gmail.com>
2019-09-18 21:01:11 -0700

    final 2.0 edits

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,6 +1,7 @@
 WORKSHOP_NAME=lab-tekton-pipelines
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:latest
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:2.0
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=7200
 IDLE_TIMEOUT=300
+SERVER_LIMIT=50
