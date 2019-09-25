76fd477e4f1d05d6621d457ac6bc29d2f4139de6
danielhelfand <helfand.4@gmail.com>
2019-09-25 11:03:14 -0400

    changing pipeline run overview pic

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-tekton-pipelines
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:2.0
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:2.1
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=7200
