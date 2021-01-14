caec846bf98a6943d1e7986f465d79e56421188a
Natale Vinto <nvinto@redhat.com>
2021-01-14 17:09:33 +0100

    Use 4.6 tag

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-tekton-pipelines
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:latest
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-tekton-pipelines:4.6
 WORKSHOP_TITLE="OpenShift Pipelines with Tekton"
 WORKSHOP_DESCRIPTION="Developing applications using OpenShift pipelines"
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.6
